fa6a0823cd8b64947355c3b9a8fd711dc86a3a45
Vi-Linh Vu <vi-linhvu@Vi-Linhs-MacBook-Pro.local>
2022-01-09 12:05:23 -0800

    creation item styling

--- src/assets/creationsdata.js
+++ src/assets/creationsdata.js
@@ -6,15 +6,15 @@ export default [
     link: "https://github.com/vilinh/plant-recipes-react",
   },
   {
-    title: "Games",
-    desc: "A website created to host my recipes.",
-    lang: "React.js, CSS, MongoDB, Express, Node.js",
-    link: "https://github.com/vilinh/plant-recipes-react",
+    title: "Movie Hub",
+    desc: "A watchlist to keep track of what you want to watch next!",
+    lang: "React.js, CSS, TMDB API",
+    link: "https://github.com/vilinh/moviehub",
   },
   {
-    title: "Plant Recipes",
-    desc: "A website created to host my recipes.",
-    lang: "React.js, CSS, MongoDB, Express, Node.js",
-    link: "https://github.com/vilinh/plant-recipes-react",
+    title: "RGB Color Game",
+    desc: "A game to test your knowledge of RGB color values.",
+    lang: "Javascript, HTML/CSS",
+    link: "https://github.com/vilinh/guess-color-game",
   },
 ];
--- src/components/CreationItem.jsx
+++ src/components/CreationItem.jsx
@@ -6,8 +6,8 @@ export default function CreationItem({ title, desc, lang, link }) {
     <div className="creation-item">
       <div className="creation-icons">
         <i className="icon star far fa-star"></i>
-        <a href={link} target="_blank">
-          <i className="icon fas fa-link"></i>
+        <a className="icon" href={link} target="_blank">
+          <i className="fas fa-link"></i>
         </a>
       </div>
       <div className="creation-title">{title}</div>
