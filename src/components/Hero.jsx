95f0dd56a3067e92200a251a101acf24e8505754
Vi-Linh Vu <vi-linhvu@Vi-Linhs-MacBook-Pro.local>
2022-01-09 20:21:12 -0800

    removed animations

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,27 +2,27 @@ import React, { useState } from "react";
 import "../styles/hero.css";
 import peach from "../assets/Peach-amico.png";
 import { Link } from "react-scroll";
-import { useSpring, animated } from "react-spring";
+// import { useSpring, animated } from "react-spring";
 
 export default function Hero() {
-  const props = useSpring({
-    to: { marginBottom: 0, opacity: 1 },
-    from: { marginBottom: -500, opacity: 0 },
-    config: { duration: 600 },
-  });
+  // const props = useSpring({
+  //   to: { marginBottom: 0, opacity: 1 },
+  //   from: { marginBottom: -500, opacity: 0 },
+  //   config: { duration: 600 },
+  // });
 
-  const animateImage = useSpring({
-    to: { y: 0 },
-    from: { y: -25 },
-    config: { duration: 1500 },
-    loop: { reverse: true },
-  });
+  // const animateImage = useSpring({
+  //   to: { y: 0 },
+  //   from: { y: -25 },
+  //   config: { duration: 1500 },
+  //   loop: { reverse: true },
+  // });
 
   return (
     <div className="hero">
       <div className="container">
         <div className="hero-wrapper">
-          <animated.div style={props} className="hero-info">
+          <div className="hero-info">
             <h1 className="hero-title">vi-linh</h1>
             <p className="hero-desc">
               I am a college student studying computer science. I have an
@@ -45,13 +45,13 @@ export default function Hero() {
                 </a>
               </button>
             </div>
-          </animated.div>
+          </div>
 
-          <animated.div style={animateImage} className="hero-img">
+          <div className="hero-img">
             <a href="https://storyset.com/">
               <img src={peach} alt="peach"></img>
             </a>
-          </animated.div>
+          </div>
 
           <div className="star-icons">
             <Link to="about" smooth>
